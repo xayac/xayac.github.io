96a5f2e8a824ca071485bc15a5b15cb0f1d547a2
xayac <burner.3dad26f3@tryninja.io>
2019-03-29 19:41:40 +1300

    updated header scroll

--- js/AnimatedTitle.js
+++ js/AnimatedTitle.js
@@ -1,35 +1,8 @@
-    var rev = "fwd";
-    function titlebar(val){
-        var msg  = "xay.ac";
-        var res = " ";
-        var speed = 100;
-        var pos = val;
-        msg = "   "+msg+"/   ";
-        var le = msg.length;
-        if(rev == "fwd"){
-            if(pos < le){
-                pos = pos+1;
-                scroll = msg.substr(0,pos);
-                document.title = scroll;
-                timer = window.setTimeout("titlebar("+pos+")",speed);
-            }
-            else {
-                rev = "bwd";
-                timer = window.setTimeout("titlebar("+pos+")",speed);
-            }
-        }
-        else {
-            if(pos > 0) {
-                pos = pos-1;
-                var ale = le-pos;
-                scrol = msg.substr(ale,le);
-                document.title = scrol;
-                timer = window.setTimeout("titlebar("+pos+")",speed);
-            }
-            else {
-                rev = "fwd";
-                timer = window.setTimeout("titlebar("+pos+")",speed);
-            }
-        }
-    }
-    titlebar(0);
+msg = "X A Y A C";
+msg = " / / / / / / / / / / / / / / / / / / / / / " + msg;position = 0;
+function scrolltitle() {
+document.title = msg.substring(position, msg.length) + msg.substring(0, position); position++;
+if (position > msg.length) position = 0
+window.setTimeout("scrolltitle()",170);
+}
+scrolltitle();
